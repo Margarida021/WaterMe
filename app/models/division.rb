2b73d466383f76f490fa1978e75cad0374e2c29e
Armindo Matias <armindomatiasg@gmail.com>
2023-08-28 17:40:14 +0100

    validations in division model

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,4 +1,14 @@
 class Division < ApplicationRecord
   belongs_to :user
   has_many :plant_divisions, dependent: :destroy
+
+  CATEGORIES = ['Living Room', 'Bedroom', 'Kitchen', 'Patio', 'Bathroom', 'Hall', 'Balcony', 'Terrace', 'Office']
+  LIGHT_DIRECTIONS = { 'Darker' => '0 hours of sunlight',
+                        'Shade' => 'A site far away from a window, or a north-facing window',
+                        'Part sun' => 'Dappled sun throughout the day',
+                        'Full sun' => 'At least 8h of full sun'
+                      }
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :light_direction, inclusion: { in: LIGHT_DIRECTIONS }
+  validates :name, :category, :user_id, :light_direction, presence: true
 end
