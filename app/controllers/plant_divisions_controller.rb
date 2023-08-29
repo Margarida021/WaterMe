8c75ab8e6ff5ed55bf4f3fb1edf08139d6c066c8
Margarida Almeida <margarida.almeida.92@gmail.com>
2023-08-29 16:14:06 +0100

    small changes to plant divisions controller

--- app/controllers/plant_divisions_controller.rb
+++ app/controllers/plant_divisions_controller.rb
@@ -1,22 +1,27 @@
 class PlantDivisionsController < ApplicationController
+  before_action :set_plant_division, only: [:show, :edit, :update, :destroy]
+
   def show
-    @plant_division = PlantDivision.find(params[:id])
   end
 
   def edit
-    @plant_division = PlantDivision.find(params[:id])
   end
 
   def update
-    @plant_division = PlantDivision.find(params[:id])
     @plant_division.update(plant_division_params)
-
+    redirect_to plant_divisions_path
   end
 
   def destroy
+    @restaurant.destroy
+    redirect_to plant_division_path, status: :see_other
   end
 
   private
+
+  def set_plant_division
+    @plant_division = PlantDivision.find(params[:id])
+  end
 
   def plant_division_params
     params.require(:plant_division).permit(:photos)
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get 'dashboard', to: 'pages#dashboard'
 
   resources :plants
+  resources :plant_divisions
 end
