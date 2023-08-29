c0f441b2198715cd4240ebf93a63c1fa99eb9dcf
Margarida Almeida <margarida.almeida.92@gmail.com>
2023-08-29 15:56:19 +0100

    first commit to this branch

--- app/controllers/plant_divisions_controller.rb
+++ app/controllers/plant_divisions_controller.rb
@@ -1,2 +1,24 @@
 class PlantDivisionsController < ApplicationController
+  def show
+    @plant_division = PlantDivision.find(params[:id])
+  end
+
+  def edit
+    @plant_division = PlantDivision.find(params[:id])
+  end
+
+  def update
+    @plant_division = PlantDivision.find(params[:id])
+    @plant_division.update(plant_division_params)
+
+  end
+
+  def destroy
+  end
+
+  private
+
+  def plant_division_params
+    params.require(:plant_division).permit(:photos)
+  end
 end
