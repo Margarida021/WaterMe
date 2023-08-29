329c9da78e8c0048f9924350129d1c4db00dd350
Armindo Matias <armindomatiasg@gmail.com>
2023-08-29 14:38:59 +0100

    beggining of the seeds and api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,8 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    @plants = Plant.all
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+# DESTROY ALL RECORDS IN THE DB
+
+User.destroy_all
+Plant.destroy_all
+Division.destroy_all
+PlantDivision.destroy_all
+
+# CREATE AN USER
+
+User.create(name: "Armindo", email: "admin@lewagon.pt", password: "123456")
+
+# POPULATING DB WITH PLANTS FROM TREFLE API
+
+url = "https://perenual.com/api/species-list?page=1&key=#{ENV["PERENUAL_API"]}"
+
+url_open = URI.open(url).read
+
+plants_data = JSON.parse(url_open)["data"]
+
+first = plants_data.first
+
+plant = Plant.new(
+  name: first["common_name"],
+  scientific_name: first["scientific_name"][0],
+  description: 'nice plant :)',
+  photo_url: first["default_image"]["original_url"],
+  watering: first["watering"],
+  light_level: first["sunlight"][0]
+)
+
+plant.save!
+
+p plants_data.first
