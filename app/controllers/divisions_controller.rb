65b8defdb2bc8081f6b8c8052e336f8147fa3c1a
Rodrigo Coelho <rodrigopcoelho2000@gmail.com>
2023-08-29 14:41:15 +0100

    feito

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -1,2 +1,5 @@
 class DivisionsController < ApplicationController
+  def new
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root to: "pages#landing"
   devise_for :users
-  root to: "pages#home"
+  resources :divisions
+  get "/home", to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
