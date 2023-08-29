76fe6a0a6f05004ced55667652ba00e82e1ee7a8
Armindo Matias <armindomatiasg@gmail.com>
2023-08-29 12:45:42 +0100

    changes on devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'dashboard', to: 'pages#dashboard'
+
+  resources :plants
 end
