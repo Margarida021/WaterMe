aa305fc141486d8b26d49149eb5f917ab32c15ca
Luana Bernardino <luanabernardino2009@hotmail.com>
2023-08-30 17:49:40 +0100

    fixed style nav

--- app/javascript/controllers/add_nav_controller.js
+++ app/javascript/controllers/add_nav_controller.js
@@ -3,10 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="add-nav"
 export default class extends Controller {
   static targets = ["content"]
-
-  connect() {
-  }
-
+  
   revealContent() {
     this.contentTarget.classList.toggle("d-none");
   }
