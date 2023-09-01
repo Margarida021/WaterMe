8a2d0138d6853bc0460f39a8a748851c33721a87
Rodrigo Coelho <rodrigopcoelho2000@gmail.com>
2023-09-01 12:15:09 +0100

    feitinho

--- app/javascript/controllers/plantphotoform_controller.js
+++ app/javascript/controllers/plantphotoform_controller.js
@@ -3,10 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="plantphotoform"
 export default class extends Controller {
   connect() {
-    
-    var childNodes = document.querySelectorAll('.lc');
+    const childNodes = document.querySelectorAll('.lc');
     if (childNodes) {
-    for (var i in childNodes) {
+    for (let i in childNodes) {
       childNodes[i].insertAdjacentHTML("afterbegin", `<img src="${}" style="width:80px; height:80px">`)
     }}
   }
