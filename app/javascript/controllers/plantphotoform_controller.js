befffadede59bbee4726676651578ca1aebed908
Rodrigo Coelho <rodrigopcoelho2000@gmail.com>
2023-09-05 16:13:45 +0100

    weather api

--- app/javascript/controllers/plantphotoform_controller.js
+++ app/javascript/controllers/plantphotoform_controller.js
@@ -3,10 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="plantphotoform"
 export default class extends Controller {
   connect() {
-    const childNodes = document.querySelectorAll('.lc');
-    if (childNodes) {
-    for (let i in childNodes) {
-      childNodes[i].insertAdjacentHTML("afterbegin", `<img src="${}" style="width:80px; height:80px">`)
-    }}
+
   }
 }
--- app/javascript/controllers/weather_controller.js
+++ app/javascript/controllers/weather_controller.js
@@ -2,15 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="weather"
 export default class extends Controller {
+
+  static values = { apiKey: String }
+
   connect() {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=38.73&lon=-9.14&appid=${ENV["WEATHERAPI"]}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=38.73&lon=-9.14&appid=${this.apiKeyValue}`;
     fetch(url)
     .then(response => response.json())
     .then((data) => {
-      console.log(data);
+      console.log(data.name);
 
       document.getElementById("degree").innerText = `${ parseInt(data.main.feels_like, 10) - 273}º`;
-      // TODO: Insert the weather info in the DOM (description, date, temperature...)
+
     });
 };
   }
