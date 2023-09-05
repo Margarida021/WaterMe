fc75fc6f2697e1feb8e2e0c8d50ba3b2fd6b3b0f
Rodrigo Coelho <rodrigopcoelho2000@gmail.com>
2023-09-05 09:37:43 +0100

    weather ap

--- app/javascript/controllers/weather_controller.js
+++ app/javascript/controllers/weather_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="weather"
 export default class extends Controller {
   connect() {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=38.73&lon=-9.14&appid=ab9b11880ba004ac5b45768d05fa918b`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=38.73&lon=-9.14&appid=${ENV["WEATHERAPI"]}`;
     fetch(url)
     .then(response => response.json())
     .then((data) => {
