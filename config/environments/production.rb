3140448d94be8ee22eec212d8ed81d1e3b763414
Margarida Almeida <margarida.almeida.92@gmail.com>
2023-09-05 17:41:57 +0100

    change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,8 +47,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
-  config.to_prepare { Devise::SessionsController.force_ssl }
-  config.to_prepare { Devise::RegistrationsController.force_ssl }
+  config.action_mailer.default_url_options = { host: 'https://www.appwater.me/' }
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
